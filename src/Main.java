667ec6122707cce9d3d342d4e3017ad7fc73ed20
TheManufacturer <145007057+TheManufacturer@users.noreply.github.com>
2023-11-02 16:17:39 +0100

    Commit Esercizio3

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            try {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-                System.out.print("Inserisci un numero intero: ");
-                int numero = scanner.nextInt();
+        System.out.println("Inserisci un indice ");
 
-                System.out.println("Hai inserito un numero intero: " + numero/0);
-                break;
+        int indice = scanner.nextInt();
 
-            } catch (ArithmeticException e) {
-
-                System.out.println("Errore: Stai dividendo per zero... Riprova.");
-                scanner.next();
-            }
-        }
-
+        divisione(arr, indice);
         scanner.close();
+
+    }
+
+    private static void divisione(int[] array, int indice) {
+        
+        try{
+
+        int denomin = 1;  //qui mettiamo [zero -->0] per causare l'errore,volendo possiamo gestire anche questo  
+
+        int divisione = array[indice] / denomin;
+
+        System.out.println(divisione);
+
+        }catch(java.lang.ArithmeticException e){
+            System.out.println("divisione per zero non fattibile");
+
+        }catch( ArrayIndexOutOfBoundsException d){
+            System.out.println("Indice non valido");
+
+        }
     }
 }
 
 /*
- * 
- * Scrivere una funzione che provi a dividere un numero per 0 e catturi
- * l'eccezione leggendone il messaggio.
- * 
+ * Scrivere una funzione che accetti un array in input e provi a dividere un
+ * numero dell'array per 0 e gestisca sia l'eccezione della divisione che quella
+ * di indice non presente nell'array, leggendone il messaggio. Eseguire sempre
+ * un blocco di codice scrivendo un messaggio in console.
  */
