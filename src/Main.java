6f4051c887300e5cb897b1af578d4edbf35e1182
Antonio <antonio.mario.demarco@gmail.com>
2023-11-02 11:35:48 +0100

    Esercizio 2 JP try_catch

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,33 @@
 import java.util.Scanner;
-import java.util.InputMismatchException; //la usiamo per l'errore input 
+import java.util.InputMismatchException;
 
 public class Main {
-
-    public static boolean isNumeroNelRange(int numero, int min, int max) {
-        if (numero >= min && numero <= max) {
-            return true;
-        } else {
-            throw new IllegalArgumentException("Il numero non è nel range da " + min + " a " + max);
-        }
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    
-        try {
-            System.out.print("Inserisci un numero intero da controllare: ");
-            int numero = scanner.nextInt();
-            System.out.println("Hai inserito un numero intero: " + numero);
-    
-            System.out.print("Inserisci il limite superiore (max): ");
-            int max = scanner.nextInt();
-    
-            System.out.print("Inserisci il limite inferiore (min): ");
-            int min = scanner.nextInt();
-    
-            isNumeroNelRange(numero, min, max);
-            System.out.println("Il numero --> " + numero + " è nel range da " + min + " a " + max);
-    
-        } catch (InputMismatchException e) {
-            System.out.println("Errore: Non hai inserito un numero intero.");
-            
-        } catch (IllegalArgumentException e) {
-            System.out.println("Errore: " + e.getMessage());
+
+        while (true) {
+            try {
+
+                System.out.print("Inserisci un numero intero: ");
+                int numero = scanner.nextInt();
+
+                System.out.println("Hai inserito un numero intero: " + numero);
+                break;
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("Errore: Non hai inserito un numero intero. Riprova.");
+                scanner.next();
+            }
         }
-    
+
         scanner.close();
     }
-    
 }
 
 /*
- * Scrivere una funzione che controlli se un numero int è in un determinato
- * range.
- * Se lo è ritorna true se non lo è lancia un'eccezione
+ * 
+ * Scrivere una funzione che controlli se un carattere è un numero altrimenti
+ * lanciare una eccezione.
+ * 
  */
