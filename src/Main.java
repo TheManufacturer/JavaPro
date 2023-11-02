5c0a4382d883b95c7923f61b04a628b2950cd298
Antonio <antonio.mario.demarco@gmail.com>
2023-11-02 13:06:33 +0100

    Esercizio 2 JP OK

--- src/Main.java
+++ src/Main.java
@@ -9,46 +9,33 @@ public class Main {
 
         do {
             try {
-                
+
                 System.out.print("Inserisci un carattere: ");
                 a = scanner.nextLine().charAt(0);
 
                 if (Character.isDigit(a)) {
+
                     System.out.println("Hai inserito un carattere numerico: " + a);
                     isValidInput = true;
+
                 } else {
+
                     System.out.println("Errore: Il carattere inserito non è un numero. Riprova.");
+
                 }
+
             } catch (StringIndexOutOfBoundsException e) {
+
                 System.out.println("Errore: Non hai inserito un carattere. Riprova.");
+
             }
+
         } while (!isValidInput);
 
         scanner.close();
     }
 }
 
-/*
- * public class Main {
- * public static void main(String[] args){
- * try{
- * char a ='e';
- * isNumber(a);
- * System.out.println(" Il carattere  è un numero.");
- * }catch(NotANumber e ){
- * System.out.println("ERRORE: Il carattere non è un numero.");
- * }
- * }
- * 
- * public static void isNumber (char a) throws NotANumber{
- * if (!Character.isDigit(a)) {
- * throw new NotANumber();
- * }
- * }
- * 
- * }
- * 
- */
 
 /*
  * Scrivere una funzione che controlli se un carattere è un numero altrimenti
