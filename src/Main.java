8ef5672969e63c2ebe994a843492a09d1d14f7da
Antonio <antonio.mario.demarco@gmail.com>
2023-11-05 17:21:15 +0100

    Esercizio Prova OK

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,43 @@
-import java.util.Scanner;
-import java.util.InputMismatchException; //la usiamo per l'errore input 
+// import java.util.Scanner; 
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Product[] products = new Product[3];
 
-        try {       //inseriamo il blocco di codice all'interno del try
-        
-            System.out.print("Inserisci un numero intero: ");
-            int numero = scanner.nextInt();
-            System.out.println("Hai inserito un numero intero: " + numero);
+        products[0] = new Product("Peraphone", "Peras", 1, 5);
+        products[1] = new Product("Bananaphone", "Bananas", 2, 10);
+        products[2] = new Product("Albiphone", "Albicocas", 3, 15);
 
-        } catch (InputMismatchException e) {
-
-            System.out.println("Errore: Non hai inserito un numero intero.");
-            main(args);
-
-        }
-
-        scanner.close();
+        Product.printManifacturerFirstLetters(products);
+        Product.calculateAverageQuantity(products);
     }
+
 }
 
-
-
-
 /*
-Scrivere una funzione che controlli se un numero int è in un determinato range.    
-    Se lo è ritorna true se non lo è lancia un'eccezione
+ * È possibile implementare costruttori, setter, getter o qualsiasi altro metodo
+ * aggiuntivo dove sia considerato necessario.
+ * ---
+ * Dichiarare una classe Product con le seguenti variabili di istanza:
+ * name: String
+ * manufacturer: String
+ * id: long
+ * quantity: int
+ * 
+ * ---
+ * 
+ * Implementare un metodo printManifacturerFirstLetters che prende
+ * un array di oggetti Product come parametro e stampa sul terminale
+ * la prima lettera dell'attributo manufacturer di ciascun elemento dell'array.
+ * ---
+ * 
+ * Implementare un metodo principale in cui:
+ * 
+ * Viene allocato un array di 3 oggetti Product.
+ * Viene chiamato il metodo printManifacturerFirstLetters passando l'array
+ * allocato come parametro.
+ * Viene stampato a schermo il valore medio dell'attributo quantity degli
+ * elementi nell'array."
+ * 
  */
