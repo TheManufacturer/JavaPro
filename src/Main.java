bcdb8ac3c7e117af6a7e89094045a6932f3179b3
Antonio <antonio.mario.demarco@gmail.com>
2023-11-09 12:02:09 +0100

    Esercizio 5JP- ok?

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,26 @@
 
 public class Main {
     public static void main(String[] args) {
+        Integer numeratore = null;
+        Integer denominatore = null;
 
-        Product[] products = new Product[3];
-
-        products[0] = new Product("Peraphone", "Peras", 1, 5);
-        products[1] = new Product("Bananaphone", "Bananas", 2, 10);
-        products[2] = new Product("Albiphone", "Albicocas", 3, 15);
-
-        Product.printManifacturerFirstLetters(products);
-        Product.calculateAverageQuantity(products);
+        try {
+            checkNullFunction(numeratore, denominatore);
+        } catch (NullPointerException e) {
+            System.out.println("sembra tu non abbia inserito tutti i dati");
+        }
     }
 
+    public static void checkNullFunction(Integer numeratore, Integer denominatore) {
+        if (numeratore == null || denominatore == null) {
+            throw new NullPointerException();
+        }
+    }
 }
 
 /*
- * È possibile implementare costruttori, setter, getter o qualsiasi altro metodo
- * aggiuntivo dove sia considerato necessario.
- * ---
- * Dichiarare una classe Product con le seguenti variabili di istanza:
- * name: String
- * manufacturer: String
- * id: long
- * quantity: int
- * 
- * ---
- * 
- * Implementare un metodo printManifacturerFirstLetters che prende
- * un array di oggetti Product come parametro e stampa sul terminale
- * la prima lettera dell'attributo manufacturer di ciascun elemento dell'array.
- * ---
- * 
- * Implementare un metodo principale in cui:
- * 
- * Viene allocato un array di 3 oggetti Product.
- * Viene chiamato il metodo printManifacturerFirstLetters passando l'array
- * allocato come parametro.
- * Viene stampato a schermo il valore medio dell'attributo quantity degli
- * elementi nell'array."
- * 
+ * Scrivere una funzione che accetti in input un numeratore e un denominatore
+ * Controlli che il numeratore e il denominatore non siano null
+ * Usare la funzione appena scritta in due blocchi di codice dove si cattura
+ * l'eventuale eccezione.
  */
