5107133aa9efaf849a02817c9767956fa0bcc398
Antonio <antonio.mario.demarco@gmail.com>
2023-11-12 16:07:34 +0100

    Esercizio8JPHashSet OK

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,35 @@ public class Main {
     }
 
     private static void funzioneHashSet(Scanner scan) {
-        HashSet <String> set = new HashSet<>();
-        System.out.println("Quanti elementi vuoi inserire? (Lavoriamo con le stringe)");
+        HashSet<Integer> set = new HashSet<>();
+        System.out.println("Quanti elementi vuoi inserire? (Lavoriamo con Numeri)");
         int grandezza = Integer.parseInt(scan.next());
-        
-        for (int i = 0; i< grandezza; i++ ){
+
+        //Qui popoliamo il nostro HashSet set...
+
+        for (int i = 0; i < grandezza; i++) {
 
             System.out.println("Inserisci Stringa");
-            set.add(scan.next());
+            set.add(Integer.parseInt(scan.next()));
 
         }
 
-        System.out.println("La grandezza del nostro HashSet 'set' è : " + set.size() );
+        //Qui controlliamo se all'interno si 'set' c'è un determinato elemento
+        System.out.println("Inserisci il valore da controllare...");
+        int numero = scan.nextInt();
+        if (set.contains(numero)){
+            System.out.println("il numero : " + numero +  " è contenuto nel set.");
+        }else {
+            System.out.println("L'elemento " + numero + " non è contenuto nel set ");
+        }
+
+        System.out.println("La grandezza del nostro HashSet 'set' è : " + set.size());
         System.out.println("Il nostro set contiene : " + set);
     }
 }
 
 /*
- * Scrivere una funzione che restituisca un hashset riempito
- * Leggere l'hashSet e stampre grandezza ed elementi
+ * Scrivere una funzione che restituisca un HashSet riempito
+ * Creare un oggetto dello stesso tipo inserito nell'HashSet e popolarlo
+ * Verificare che l' elemento sia parte del Set e stampare il risultato
  */
