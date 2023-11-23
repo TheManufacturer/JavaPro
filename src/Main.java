4afe0c31d5f5a636e7685bbd10417bcaed4986b4
TheManufacturer <antonio.mario.demarco@gmail.com>
2023-11-23 20:13:40 +0100

    ok?

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,29 @@
-import java.time.OffsetDateTime;
+ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) { 
 
-                // Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z + Stampa
-                OffsetDateTime dateConv = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-                String dataConvertita = dateConv.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
+        OffsetDateTime dateConv = OffsetDateTime.parse("2023-03-01T13:00:00Z");  
 
-                System.out.println("La Stringa --> 2023-03-01T13:00:00Z diventa : " + dataConvertita);
+        //diamo un  pattern come richiesto "01" dd
+        DateTimeFormatter WorkFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
-                // Ottieni metodoGiorno + stampa
+        //Formattiamo in una variabile String
+        String DataWorkata = dateConv.format(WorkFormat);
 
-                metodoGiorno(dateConv);
+        //Stampiamo il risultato ottenuto
+        System.out.println("\n La Stringa --> 2023-03-01T13:00:00Z diventa : " + DataWorkata); 
 
-                // Ottieni metodoGiornoSettimana + stampa
-                metodoGiornoSettimana(dateConv);
-
-                // Ottieni metodoMese (con MMMM è esteso) + stampa
-
-                metodoMese(dateConv);
-
-                // Ottieni MetodoAnno (yyyy) + stampa
-
-                MetodoAnno(dateConv);
-
-        }
-
-        private static void MetodoAnno(OffsetDateTime dateConv) {
-                String datAnno = dateConv.format(DateTimeFormatter.ofPattern("yyyy"));
-                System.out.println("L'anno : " + datAnno);
-        }
-
-        private static void metodoMese(OffsetDateTime dateConv) {
-                String dataMese = dateConv.format(DateTimeFormatter.ofPattern("MMMM"));
-                System.out.println("Il giorno : " + dataMese);
-        }
-
-        private static void metodoGiornoSettimana(OffsetDateTime dateConv) {
-                String dataGiornoSettimana = dateConv.format(DateTimeFormatter.ofPattern("EEEE"));
-                System.out.println("Il giorno : " + dataGiornoSettimana);
-        }
-
-        public static void metodoGiorno(OffsetDateTime dateConv) {
-                String dataGiorno = dateConv.format(DateTimeFormatter.ofPattern("dd"));
-                System.out.println("Il giorno : " + dataGiorno);
-        }
+    }
 }
 
 /*
- * Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
- * ottieni l'anno
- * ottieni il mese
- * ottieni il giorno
- * ottieni il giorno della settimana
- * Stampa i risultati sulla console
+ * Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
+ * 
+ * Formatta la data ottenendo 01 marzo 2023
+ * 
+ * Stampa sulla console
+ * 
  * Crea dei test per questo esercizio
  */
