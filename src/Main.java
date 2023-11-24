3d4f00843eed39dbc1fc70ecefc806797f4524a2
TheManufacturer <antonio.mario.demarco@gmail.com>
2023-11-24 11:58:54 +0100

    Esercizio25JPTest5

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,63 @@
-
- import java.time.OffsetDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
 
 public class Main {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
 
-        OffsetDateTime dateConv = OffsetDateTime.parse("2023-03-01T13:00:00Z");  
+        // Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z + Stampa per
+        // confronto
 
-        //diamo un  pattern come richiesto "01" dd
-        DateTimeFormatter WorkFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        OffsetDateTime dateConv = OffsetDateTime.parse("2023-03-01T13:00:00Z");
 
-        //Formattiamo in una variabile String
-        String DataWorkata = dateConv.format(WorkFormat);
+        String dataConvertita = dateConv.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
 
-        //Stampiamo il risultato ottenuto
-        System.out.println("\n La Stringa --> 2023-03-01T13:00:00Z diventa : " + DataWorkata); 
+        System.out.println("\n La Stringa --> 2023-03-01T13:00:00Z \n diventa : " + dataConvertita);
 
+        // Aggiungi 7 giorni + stampa
+
+        metodoAddSevenD(dateConv);
+
+        // Aggiungi un mese (con MMMM è esteso) + stampa
+
+        metodoAddOneM(dateConv);
+
+        // Aggiungi un anno anno (yyyy) + stampa
+        metodoAddOneY(dateConv);
+
+        // Stampa il risultato localizzata per l'Italia
+        metodoDateLocalized(dateConv);
+    }
+
+    public static void metodoDateLocalized(OffsetDateTime dateConv) {
+        String dataLocalizzata = dateConv.format(DateTimeFormatter.ofPattern("yyyy", Locale.ITALY));
+        System.out.println("\n Data in formato ITALY : " + dataLocalizzata);
+    }
+
+    public static void metodoAddOneY(OffsetDateTime dateConv) {
+        OffsetDateTime dataRevisionataY = dateConv.plusYears(1);
+        System.out.println("\n L'anno aumentato di 1 : " + dataRevisionataY);
+
+        String dataYearFormattata = dataRevisionataY.format(DateTimeFormatter.ofPattern("yyyy"));
+
+        System.out.println("\n L'anno aumentato di 1 (formattato): " + dataYearFormattata);
+    }
+
+    public static void metodoAddOneM(OffsetDateTime dateConv) {
+        System.out.println("\n Il mese aumentato di 1 : " + dateConv.plusMonths(1));
+    }
+
+    public static void metodoAddSevenD(OffsetDateTime dateConv) {
+        System.out.println("\n Il giorno aumentato di 7: " + dateConv.plusDays(7));
     }
 }
 
 /*
- * Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
- * 
- * Formatta la data ottenendo 01 marzo 2023
- * 
- * Stampa sulla console
- * 
+ * Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
+ * aggiungi un anno
+ * sottrai un mese
+ * aggiungi 7 giorni
+ * Stampa il risultato localizzata per l'Italia
  * Crea dei test per questo esercizio
  */
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,9 +1,12 @@
 import static org.junit.Assert.assertEquals;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import org.junit.Test;
 
 public class MainTest {
+    Main mainT = new Main();
 
     @Test
     public void testFirstPrint() {
@@ -21,4 +24,40 @@ public class MainTest {
         String expected = "01 marzo 2023";
         assertEquals("Error with Format Date", expected, actual);
     }
+
+
+    @Test
+    public void testMetodoAddSevenD() {
+        OffsetDateTime dateConv = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        mainT.metodoAddSevenD(dateConv);
+        OffsetDateTime expected = dateConv.plusDays(7);
+        assertEquals(dateConv.plusDays(7), expected);
+    }
+
+    @Test
+    public void testMetodoAddOneM() {
+        OffsetDateTime dateConv = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        mainT.metodoAddOneM(dateConv);
+        OffsetDateTime expected = dateConv.plusMonths(1); 
+        assertEquals(dateConv.plusMonths(1), expected);
+    }
+
+    @Test
+    public void testMetodoAddOneY() {
+        OffsetDateTime dateConv = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        mainT.metodoAddOneY(dateConv);
+        OffsetDateTime expected = dateConv.plusYears(1); 
+        assertEquals(dateConv.plusYears(1), expected);
+    }
+
+    // checkLocalized
+    @Test
+    public void testMetodoDateLocalized() {
+        OffsetDateTime dateConv = OffsetDateTime.parse("2024-03-01T13:00:00Z");
+        String expected = dateConv.format(DateTimeFormatter.ofPattern("yyyy", Locale.ITALY)); 
+        String actual = "2024";
+        assertEquals(actual, expected);
+    }
 }
+
+/*testMetodoDateLocalized--> 2024-03-01T13:00Z */
